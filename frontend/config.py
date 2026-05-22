6fc9807f4e157a1710b2fffb449a07ba5d0ea33d
mystic0137 <cryptobotnewgls@gmail.com>
2026-05-22 22:19:09 +0530

    frontend: import EXPORT_FIELDS from src.config (remove duplication)

--- frontend/config.py
+++ frontend/config.py
@@ -1,13 +1,1 @@
-EXPORT_FIELDS = [
-    "customer_id",
-    "customer_name", 
-    "email",
-    "phone",
-    "num_passengers",
-    "route",
-    "booking_origin",
-    "haul_type",
-    "wants_extra_baggage",
-    "wants_preferred_seat",
-    "wants_in_flight_meals",
-]
+from src.config import EXPORT_FIELDS
